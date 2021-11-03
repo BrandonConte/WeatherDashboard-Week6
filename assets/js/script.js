78cd2c23f1e80c222b053bb9c4dbb2e7fcefc005
BrandonConte <bconte950@gmail.com>
2021-11-03 19:33:01 -0400

    function to grab the weather finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,34 @@ function renderWeather() {
     rowEl.classList = 'row';
     forecastEl.appendChild(rowEl);
 
+    for (i=0;i<5;i++) {
+        var cardEl = document.createElement("div");
+        var tempEl = document.createElement("div");
+        var windEl = document.createElement("div");
+        var humidityEl = document.createElement("div");
+        var dateEl = document.createElement("div");
+        var iconEl = document.createElement("div");
+
+        cardEl.classList = "col-2 card";
+        tempEl.classList = "key";
+        windEl.classList = "key";
+        humidityEl.classList = "key";
+        dateEl.classList = "key";
+        iconEl.classList = "key";
+        
+        tempEl.textContent = "Temp: ";
+        windEl.textContent = "Wind: ";
+        humidityEl.textContent = "Humidity: ";
+        dateEl.textContent = "Date: ";
+        iconEl.textContent = "Icon: ";
+
+        rowEl.appendChild(cardEl);
+        cardEl.appendChild(tempEl);
+        cardEl.appendChild(windEl);
+        cardEl.appendChild(humidityEl);
+        cardEl.appendChild(dateEl);
+        cardEl.appendChild(iconEl);
+
+    }
 }
+console.log(renderWeather);
