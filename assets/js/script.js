9883565c4e87f30cf1b186e137da9d670c58a74d
BrandonConte <bconte950@gmail.com>
2021-11-03 21:52:03 -0400

    Added fa-icons that work well. Just need to fix the current weather icon to match the others

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,52 +3,21 @@ var query1 = "https://api.openweathermap.org/data/2.5/weather?q=";
 var query2 = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 var exclude = "&exclude=minutely,hourly,alerts&units=imperial";
 
+
 var cities = document.getElementById('cities')
 var searchButton = document.getElementById('search');
 
 function renderWeather() {
-    let forecastEl = document.getElementById("forecast");
-    let rowEl = document.createElement('div');
-
-    rowEl.classList = 'row';
-    forecastEl.appendChild(rowEl);
-
-    for (i=0;i<5;i++) {
-        var cardEl = document.createElement("div");
-        var tempEl = document.createElement("div");
-        var windEl = document.createElement("div");
-        var humidityEl = document.createElement("div");
-        var dateEl = document.createElement("div");
-        var iconEl = document.createElement("div");
-
-        cardEl.classList = "col-2 card";
-        tempEl.classList = "key";
-        windEl.classList = "key";
-        humidityEl.classList = "key";
-        dateEl.classList = "key";
-        iconEl.classList = "key";
-        
-        tempEl.textContent = "Temp: ";
-        windEl.textContent = "Wind: ";
-        humidityEl.textContent = "Humidity: ";
-        dateEl.textContent = "Date: ";
-        iconEl.textContent = "Icon: ";
-
-        rowEl.appendChild(cardEl);
-        cardEl.appendChild(tempEl);
-        cardEl.appendChild(windEl);
-        cardEl.appendChild(humidityEl);
-        cardEl.appendChild(dateEl);
-        cardEl.appendChild(iconEl);
-
-    }
+    
 }
 // console.log(renderWeather);
 
 function renderCity() {
     var citySearch = document.getElementById('citySearch').value;
-    document.getElementById('city').innerText = citySearch + moment().format('(MM/DD/YY)');
+    document.getElementById('city').innerText = citySearch + moment().format(' MM/DD/YY');
     console.log(citySearch);
+    var forecastEl = document.getElementById('forecast');
+        forecastEl.innerHTML = '';
 
     fetch(query1 + citySearch + apiKey)
     .then(function(response){
@@ -65,17 +34,83 @@ function renderCity() {
     })
     .then(function(data) {
         console.log(data);
-        var temp = data.current.temp;
-        var wind = data.current.wind_speed;
-        var humidity = data.current.humidity;
-        var uvIndex = data.current.uvi;
+        let forecastEl = document.getElementById("forecast");
+        let rowEl = document.createElement('div');
+
+    rowEl.classList = 'row';
+    forecastEl.appendChild(rowEl);
+
+
+        for (i=1;i<6;i++) {
+
+            var temp = data.daily[i].temp.day;
+            var wind = data.daily[i].wind_speed;
+            var humidity = data.daily[i].humidity;
+            var uvIndex = data.daily[i].uvi;
+            var iconCode = data.daily[i].weather[0].icon;
+            var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"; 
+            var cardEl = document.createElement("div");
+            var tempEl = document.createElement("div");
+            var windEl = document.createElement("div");
+            var humidityEl = document.createElement("div");
+            var dateEl = document.createElement("div");
+            var iconEl = document.createElement("div");
+            var forecastContainerEl = document.createElement("div");
+    
+            cardEl.classList = "col-8 card";
+            tempEl.classList = "mainStats";
+            windEl.classList = "mainStats";
+            humidityEl.classList = "mainStats";
+            dateEl.classList = "mainStats";
+            iconEl.classList = "mainStats";
+
+            
+            
+            tempEl.textContent = "Temp: " + temp;
+            windEl.textContent = "Wind: " + wind;
+            humidityEl.textContent = "Humidity: " + humidity;
+            dateEl.textContent = "Date: " + moment().add(i, 'days').format('MM/DD/YY');
+            iconEl.textContent = "Icon: " + $('#wIcon').attr('src', iconUrl);
+            
+            var weatherId = data.daily[i].weather[0].id;
+            var iconEl = document.createElement("span");
+            
+    
+            if (weatherId > 800) {
+                iconEl.innerHTML ='<i class="fas fa-cloud-sun fa-3x"></i>';
+    
+            } else if (weatherId === 800) {
+                iconEl.innerHTML ='<i class="fas fa-sun fa-3x"></i>';
+            } else if (weatherId > 700) {
+                iconEl.innerHTML ='<i class="fas fa-smog"></i>';
+            } else if (weatherId >= 600) {
+                iconEl.innerHTML ='<i class="fas fa-snowflake"></i>';
+            } else if (weatherId >= 500) {
+                iconEl.innerHTML ='<i class="fas fa-cloud-showers-heavy"></i>';
+            } else if (weatherId >= 300) {
+                iconEl.innerHTML ='<i class="fas fa-cloud-rain"></i>';
+            } else {
+                iconEl.innerHTML ='<i class="fas fa-poo-storm"></i>';
+            }
+    
+            forecastContainerEl.appendChild(iconEl);
+    
+            
+            rowEl.appendChild(cardEl);
+            cardEl.appendChild(tempEl);
+            cardEl.appendChild(windEl);
+            cardEl.appendChild(humidityEl);
+            cardEl.appendChild(dateEl);
+            cardEl.appendChild(iconEl);
+    
+        }
         
         document.getElementById('temperature').textContent = 'Temp: ' + temp + " F";
         document.getElementById('wind').textContent = 'Wind: ' + wind + ' MPH';
         document.getElementById('humidity').textContent = 'Humidity: ' + humidity + " %";
         document.getElementById('uvIndex').textContent = 'UV Index: ' + uvIndex;
-
-    })
+        
+    });
     renderWeather();
 });
 
